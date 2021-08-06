a1c032836b16bc4cfdac4ae2c2495b0c0276f35c
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2021-08-06 23:40:58 +0800

    load/unload

--- cog_todolist.py
+++ cog_todolist.py
@@ -1,6 +1,9 @@
 import asyncio
 
 class TestList:
-    def __init__(self, ctx):
-        self.bot = ctx.bot
+    def __init__(self, bot):
+        self.bot = bot
         self.queue = asyncio.Queue()
+
+def setup(bot):
+    bot.add_cog(TestList(bot))
--- pydiscord.py
+++ pydiscord.py
@@ -9,8 +9,7 @@ NOTPLAYING = '前提是我有播東西啊~(っ・д・)っ\n'
 MISSINGARG = '求後續(´・ω・`)\n'
 POSINT = '正整數啦!  (´_ゝ`)\n'
 MEME = ['不要停下來阿', '卡其脫離太', '穿山甲', '卡打掐', '豆花', '阿姨壓一壓', 'Daisuke']
-COG_LIST = ['cog_trigger_meme', 'cog_ytdl', 'cog_pixivrec', 'cog_headCounter', 'cog_todolist']
-CUR_LOADED = []
+COG_LIST = {'cog_trigger_meme', 'cog_ytdl', 'cog_pixivrec', 'cog_headCounter', 'cog_todolist'}
 
 def main():
     absFilePath = os.path.abspath(__file__)
@@ -26,70 +25,77 @@ def main():
     async def on_ready():
         await client.change_presence(activity = discord.Game('debugger(殺蟲劑)'))
         client.load_extension("cog_mainbot")
+        client.LOADED_COG = set()
         print('\nBot is now online.')
 
     @client.command()
     async def reload(ctx):
-        #main doesnt need reload = =
+        # main doesnt need reload = =
         #client.reload_extension("cog_mainbot")
         suc = 0
-        for c in COG_LIST:
-            try:
-                client.reload_extension(c)
-                suc += 1
-            except:
-                pass
+        for c in client.LOADED_COG:
+            client.reload_extension(c)
+            suc += 1
         await ctx.send(f'reload {suc} cog done')
-        print(f'reloaded {suc}')
+        print(f'[C] reloaded {suc}')
 
     @client.command()
     async def load(ctx, *args):
         suc = 0
         fal = 0
-        if '-l' in args:
-            await ctx.send(', '.join(COG_LIST))
+        if (not args) or '-l' in args:
+            await ctx.send(',  '.join(COG_LIST))
             return
+
         elif '-a' in args:
             for cl in COG_LIST:
                 suc+=1
                 client.load_extension(cl)
-            #await ctx.send('full load completed')
-            #return
+
         else:
             for cog in args:
-                if cog in COG_LIST:
+                if cog in client.LOADED_COG:
+                    fal+=1
+                    print(f"{cog} already loaded")
+                elif cog in COG_LIST:
                     suc+=1
-                    CUR_LOADED.append(cog)
+                    client.LOADED_COG.add(cog)
                     client.load_extension(cog)
                     print(f"{cog} load done")
                 else:
                     fal+=1
                     print(f"{cog} not exist")
         await ctx.send(f'load {suc} done\nload {fal} failed')
-        print('\nBot cog loaded')
+        print('[C] loaded, now : ', client.LOADED_COG)
 
     @client.command()
     async def unload(ctx, *args):
         suc = 0
         fal = 0
-        if '-l' in args:
-            await ctx.send(f"current loaded : {', '.join(CUR_LOADED)}")
+        if (not args) or ('-l' in args):
+            await ctx.send(f"current loaded : {',  '.join(client.LOADED_COG)}")
             return
+
         elif '-a' in args:
-            for cl in CUR_LOADED:
+            for cl in client.LOADED_COG:
                 client.unload_extension(cl)
+            # reset loaded set
+            client.LOADED_COG = set()
             await ctx.send('full unload completed')
-        try:
-            for cog in args:
-                if cog in COG_LIST:
-                    suc+=1
-                    client.unload_extension(cog)
-                    print(f"{cog} unload done")
-                else:
-                    fal+=1
-                    print(f"{cog} not loaded")
-        except:
-            raise 
+        
+        for cog in args:
+            if cog in client.LOADED_COG:
+                suc+=1
+                client.LOADED_COG.remove(cog)
+                client.unload_extension(cog)
+                print(f"{cog} unload done")
+            else:
+                fal+=1
+                print(f"{cog} not exist")
+        await ctx.send(f'unload {suc} done\nunload {fal} failed')
+        print('[C] unloaded, now : ', client.LOADED_COG)
+
+    # Game Start!
     client.run(TOKEN)
 
 if __name__ == "__main__":
