89428c5fef55d58af24fa6a208586651fcd46b20
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2021-08-16 01:59:54 +0800

    can get data no sound

--- cog_temp.py
+++ cog_temp.py
@@ -7,7 +7,7 @@ import sys
 import traceback
 from async_timeout import timeout
 from functools import partial
-from SongData import Loader, Song
+from SongData import *
 import youtube_dl
 
 ffmpeg_opts = {
@@ -30,7 +30,7 @@ class GuildMusicPlayer:
     When the bot disconnects from the Voice it's instance will be destroyed.
     """
 
-    __slots__ = ('bot', '_guild', '_channel', '_cog', 'queue', 'next', 'current', 'np', 'nans', 'volume')
+    __slots__ = ('bot', '_guild', '_channel', '_cog', 'sngQueue', 'toggleNext', 'current', 'np', 'nans', 'volume')
 
     def __init__(self, ctx):
         self.bot = ctx.bot
@@ -38,11 +38,10 @@ class GuildMusicPlayer:
         self._channel = ctx.channel
         self._cog = ctx.cog
 
-        self.queue = asyncio.Queue()
-        self.next = asyncio.Event()
+        self.sngQueue = asyncio.Queue()
+        self.toggleNext = asyncio.Event()
 
         self.np = None  # Now playing message
-        self.nans = set()
         self.volume = .5
         self.current = None
         
@@ -53,43 +52,13 @@ class GuildMusicPlayer:
         await self.bot.wait_until_ready()
 
         while not self.bot.is_closed():
-            self.next.clear()
+            self.toggleNext.clear()
+            source = await self.sngQueue.get()
+            await self._channel.send(f'Now Playing: {source.title}')
 
-            try:
-                # Wait for the next song. If we timeout cancel the player and disconnect...
-                async with timeout(400):  # 5 minutes...
-                    source = await self.queue.get()
-            except asyncio.TimeoutError:
-                print("loop ended")
-                return self.destroy(self._guild)
-
-            if not isinstance(source, YTDLSource):
-                # Source was probably a stream (not downloaded)
-                # So we should regather to prevent stream expiration
-                try:
-                    source = await YTDLSource.regather_stream(source, loop=self.bot.loop)
-                except Exception as e:
-                    await self._channel.send(f'There was an error processing your song.\n'
-                                             f'```css\n[{e}]\n```')
-                    continue
-
-            source.volume = self.volume
-            self.current = source
-
-            self._guild.voice_client.play(source, after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
-            self.np = await self._channel.send(f'**Now Playing:** `{source.title}` requested by '
-                                               f'`{source.requester}`')
-            await self.next.wait()
-
-            # Make sure the FFmpeg process is cleaned up.
-            source.cleanup()
-            self.current = None
-
-            try:
-                # We are no longer playing this song...
-                await self.np.delete()
-            except discord.HTTPException:
-                pass
+            self._guild.voice_client.play(discord.FFmpegPCMAudio(source.url, **ffmpeg_opts), after=lambda _: self.bot.loop.call_soon_threadsafe(self.toggleNext.set))
+            
+            await self.toggleNext.wait()
 
     def destroy(self, guild):
         """Disconnect and cleanup the player."""
@@ -198,12 +167,8 @@ class Musicv2(commands.Cog):
             await ctx.invoke(self.connect_)
 
         gplayer = self.get_player(ctx)
-
-        # If download is False, source will be a dict which will be used later to regather the stream.
-        # If download is True, source will be a discord.FFmpegPCMAudio with a VolumeTransformer.
-        await gplayer.loader.load_song(search)
-
-        #await player.queue.put(song)
+        strmSong = await self.loader.load_song(search)
+        await gplayer.sngQueue.put(strmSong)
 
     @commands.command(name='pause')
     async def pause_(self, ctx):
@@ -256,11 +221,11 @@ class Musicv2(commands.Cog):
             return await ctx.send('I am not currently connected to voice!', delete_after=20)
 
         player = self.get_player(ctx)
-        if player.queue.empty():
+        if player.sngQueue.empty():
             return await ctx.send('There are currently no more queued songs.')
 
         # Grab up to 5 entries from the queue...
-        upcoming = list(itertools.islice(player.queue._queue, 0, 10))
+        upcoming = list(itertools.islice(player.sngQueue._queue, 0, 10))
 
         fmt = '\n'.join(f'**`{i["title"]}`**' for i in upcoming)
         embed = discord.Embed(title=f'Upcoming - Next {len(upcoming)}', description=fmt)
