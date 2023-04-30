1b8f341ff58ce093e08e874c16db9225efb06b64
jasonZzz <48216420+jasonkao402@users.noreply.github.com>
2023-04-30 19:06:34 +0800

    ok good joke done beta test

--- cog/okgoodjoke.py
+++ cog/okgoodjoke.py
@@ -20,10 +20,10 @@ def localRead():
         emoji2id.update((alias, i) for alias in setsys_tmp[i].split())
     print(emoji2id)
     
-def nameChk(s) -> tuple:
-    for name in emoji2id:
-        if name in s: return emoji2id[name], name
-    return -1, ''
+def nameChk(s):
+    for emoji in emoji2id:
+        if emoji in s: return emoji2id[emoji]
+    return -1
 
 class okgoodjoke(commands.Cog):
     __slots__ = ('bot')
@@ -35,13 +35,19 @@ class okgoodjoke(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
+        emj = str(payload.emoji)
         if payload.guild_id == 477839636404633600:
             ch = self.bot.get_partial_messageable(payload.channel_id, guild_id=payload.guild_id)
             msg = await ch.fetch_message(payload.message_id)
-            mdc = {emoji.__str__ : emoji.count for emoji in msg.reactions}
-            for k, v in mdc.items():
-                if v >= 2:
-                    ch.send()
+            uid = msg.author.id
+            mdc = {str(emoji) : emoji.count for emoji in msg.reactions}[emj]
+            if mdc == 3 and (eid:=nameChk(emj)) != -1:
+                if uid not in emojiArr.index:
+                    emojiArr.loc[uid] = 0
+                emojiArr.loc[uid].iloc[eid] += 1
+                t = emojiArr.iloc[:,eid].sort_values(ascending=False).head(5)
+                sb = sepLines((f'{wcformat(self.bot.get_user(i).name)}: {v}'for i, v in zip(t.index, t.values)))
+                return await ch.send(f'{emj} Scoreboard:\n```{sb}```', reference=msg)
                 
 async def setup(bot):
     localRead()
@@ -49,5 +55,5 @@ async def setup(bot):
     
 async def teardown(bot):
     print('emoji saved')
-    print(emojiArr)
+    # print(emojiArr)
     emojiArr.to_csv('./acc/emojiArr.csv')
